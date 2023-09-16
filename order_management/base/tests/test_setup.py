c0004f2c0ff5221eae68f409618bcb27de86d8a5
LawrenceChege <lawrencechege.s@gmail.com>
2023-09-16 13:56:27 +0300

    setup test setup

---
+++ order_management/base/tests/test_setup.py
@@ -0,0 +1,17 @@
+import json
+
+from mixer.backend.django import mixer
+from rest_framework.test import APITestCase, APIRequestFactory, APIClient
+
+
+class TestSetUp(APITestCase):
+	"""
+		Setup data for Customer Models
+	"""
+	def setUp(self):
+		self.client = APIClient()
+		self.factory = APIRequestFactory()
+		self.state = mixer.blend('base.State', name="Active")
+
+	def tearDown(self):
+		return super().tearDown()
