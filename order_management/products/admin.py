38144d51d24d41ad748da66c495ad70fbec4cb38
LawrenceChege <lawrencechege.s@gmail.com>
2023-09-17 13:15:36 +0300

    update product models and admin

--- order_management/products/admin.py
+++ order_management/products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Product
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	"""
+	Product model admin. Defines the fields to display and which ones are searchable
+	"""
+	list_filter = ('date_created',)
+	list_display = ('name', 'price', 'quantity', 'date_modified', 'date_created')
+	search_fields = ('name',)
--- order_management/products/models.py
+++ order_management/products/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from order_management.base.models import BaseModel, State, NamedModel, Currency, Category
+from base.models import BaseModel, State, NamedModel, Currency, Category
 
 
 class Product(NamedModel):
 	"""
 	Defines model
 	"""
-	category = models.ForeignKey(Category, on_delete=models.CASCADE())
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	price = models.DecimalField(verbose_name="Price of product", max_digits=25, decimal_places=2, default=0.00)
-	quantity = models.IntegerField(verbose_name="Number of products", max_length=25)
-	currency = models.ForeignKey(Currency, on_delete=models.CASCADE())
-	status = models.ForeignKey(State, default=State.default_state,  on_delete=models.CASCADE())
+	quantity = models.IntegerField(verbose_name="Number of products")
+	currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+	status = models.ForeignKey(State, default=State.default_state,  on_delete=models.CASCADE)
 	
 	class Meta(BaseModel.Meta):
 		verbose_name_plural = 'Products'
