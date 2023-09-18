198f7162819011de6dcd62bb5be1ae25f023dfce
LawrenceChege <lawrencechege.s@gmail.com>
2023-09-18 17:17:48 +0300

    add product seriaizers

--- order_management/products/backend/seriallizers.py
+++ order_management/products/backend/seriallizers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
-from order_management.products.models import Product
+from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Product
 		fields = '__All__'
+		
