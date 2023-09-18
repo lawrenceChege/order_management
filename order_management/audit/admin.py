ac0054b1a4ee356b45b318838df35ee1a21dc3f7
LawrenceChege <lawrencechege.s@gmail.com>
2023-09-18 18:34:25 +0300

    add audit admin

--- order_management/audit/admin.py
+++ order_management/audit/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from order_management.audit.models import Action
+
+
 # Register your models here.
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+	"""
+	Action model admin. Defines the fields to display and which ones are searchable
+	"""
+	list_filter = ('date_created',)
+	list_display = (
+	'user', 'action_type', 'reference', 'source_ip', 'request', 'status_code', 'trace', 'description', 'date_modified',
+	'date_created')
+	search_fields = ('reference',)
