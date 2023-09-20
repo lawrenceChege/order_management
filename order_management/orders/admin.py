392e0b86a5f38a520518a349da41b3f1d1df3688
LawrenceChege <lawrencechege.s@gmail.com>
2023-09-20 15:49:36 +0300

    add orders

--- order_management/orders/admin.py
+++ order_management/orders/admin.py
@@ -1,24 +1,5 @@
 from django.contrib import admin
+from .models import Customer, Order
 
-from .models import OrderItem, Order
-
-
-# Register your models here.
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-	"""
-	Order model admin. Defines the fields to display and which ones are searchable
-	"""
-	list_filter = ('date_created',)
-	list_display = ('customer', 'total_price', 'date_modified', 'date_created')
-	search_fields = ('name',)
-
-
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-	"""
-	Order Ite, model admin. Defines the fields to display and which ones are searchable
-	"""
-	list_filter = ('date_created',)
-	list_display = ('order', 'product', 'quantity', 'total_price', 'date_modified', 'date_created')
-	search_fields = ('product__name',)
+admin.site.register(Customer)
+admin.site.register(Order)
--- order_management/orders/views.py
+++ order_management/orders/views.py
@@ -1,26 +1,3 @@
-from rest_framework import viewsets
-from rest_framework import permissions
-from .backend.serializers import OrderItemSerializer, OrderSerializer
+from django.shortcuts import render
 
-from .models import OrderItem, Order
-
-
-class OrderViewSet(viewsets.ModelViewSet):
-	"""
-	API endpoint that allows orders to be viewed or edited.
-	"""
-	queryset = Order.objects.all()
-	serializer_class = OrderSerializer
-	permission_classes = [permissions.IsAuthenticated]
-
-
-class OrderItemViewSet(viewsets.ModelViewSet):
-	"""
-	API endpoint that allows order items to be viewed or edited.
-	"""
-	queryset = OrderItem.objects.all()
-	serializer_class = OrderItemSerializer
-	permission_classes = [permissions.IsAuthenticated]
-
-
-
+# Create your views here.
